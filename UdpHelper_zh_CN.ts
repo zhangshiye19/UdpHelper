ccc083ce1482a0c1e04c3e25e28d3a28371c5848
zhangshiye19 <zhangshiye19@outlook.com>
2022-11-14 13:52:23 +0800

    模板上传

--- UdpHelper_zh_CN.ts
+++ UdpHelper_zh_CN.ts
@@ -7,6 +7,7 @@
         <location filename="mainwindow.ui" line="14"/>
         <location filename="cmake-build-debug-visual-studio/UdpHelper_autogen/include_Debug/ui_mainwindow.h" line="249"/>
         <location filename="cmake-build-release/UdpHelper_autogen/include_Release/ui_mainwindow.h" line="267"/>
+        <location filename="cmake-build-release-mingw_qt/UdpHelper_autogen/include/ui_mainwindow.h" line="267"/>
         <source>MainWindow</source>
         <translation type="unfinished"></translation>
     </message>
@@ -14,6 +15,7 @@
         <location filename="mainwindow.ui" line="21"/>
         <location filename="cmake-build-debug-visual-studio/UdpHelper_autogen/include_Debug/ui_mainwindow.h" line="250"/>
         <location filename="cmake-build-release/UdpHelper_autogen/include_Release/ui_mainwindow.h" line="268"/>
+        <location filename="cmake-build-release-mingw_qt/UdpHelper_autogen/include/ui_mainwindow.h" line="268"/>
         <source>工具台</source>
         <translation type="unfinished"></translation>
     </message>
@@ -21,6 +23,7 @@
         <location filename="mainwindow.ui" line="27"/>
         <location filename="cmake-build-debug-visual-studio/UdpHelper_autogen/include_Debug/ui_mainwindow.h" line="251"/>
         <location filename="cmake-build-release/UdpHelper_autogen/include_Release/ui_mainwindow.h" line="269"/>
+        <location filename="cmake-build-release-mingw_qt/UdpHelper_autogen/include/ui_mainwindow.h" line="269"/>
         <source>目标IP</source>
         <translation type="unfinished"></translation>
     </message>
@@ -28,12 +31,14 @@
         <location filename="mainwindow.ui" line="37"/>
         <location filename="cmake-build-debug-visual-studio/UdpHelper_autogen/include_Debug/ui_mainwindow.h" line="252"/>
         <location filename="cmake-build-release/UdpHelper_autogen/include_Release/ui_mainwindow.h" line="270"/>
+        <location filename="cmake-build-release-mingw_qt/UdpHelper_autogen/include/ui_mainwindow.h" line="270"/>
         <source>目标端口</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="mainwindow.ui" line="47"/>
         <location filename="cmake-build-release/UdpHelper_autogen/include_Release/ui_mainwindow.h" line="271"/>
+        <location filename="cmake-build-release-mingw_qt/UdpHelper_autogen/include/ui_mainwindow.h" line="271"/>
         <source>本地服务端口：</source>
         <translation type="unfinished"></translation>
     </message>
@@ -41,6 +46,7 @@
         <location filename="mainwindow.ui" line="70"/>
         <location filename="cmake-build-debug-visual-studio/UdpHelper_autogen/include_Debug/ui_mainwindow.h" line="253"/>
         <location filename="cmake-build-release/UdpHelper_autogen/include_Release/ui_mainwindow.h" line="272"/>
+        <location filename="cmake-build-release-mingw_qt/UdpHelper_autogen/include/ui_mainwindow.h" line="272"/>
         <source>应用</source>
         <translation type="unfinished"></translation>
     </message>
@@ -48,6 +54,7 @@
         <location filename="mainwindow.ui" line="107"/>
         <location filename="cmake-build-debug-visual-studio/UdpHelper_autogen/include_Debug/ui_mainwindow.h" line="254"/>
         <location filename="cmake-build-release/UdpHelper_autogen/include_Release/ui_mainwindow.h" line="273"/>
+        <location filename="cmake-build-release-mingw_qt/UdpHelper_autogen/include/ui_mainwindow.h" line="273"/>
         <source>发送</source>
         <translation type="unfinished"></translation>
     </message>
@@ -55,6 +62,7 @@
         <location filename="mainwindow.ui" line="118"/>
         <location filename="cmake-build-debug-visual-studio/UdpHelper_autogen/include_Debug/ui_mainwindow.h" line="255"/>
         <location filename="cmake-build-release/UdpHelper_autogen/include_Release/ui_mainwindow.h" line="274"/>
+        <location filename="cmake-build-release-mingw_qt/UdpHelper_autogen/include/ui_mainwindow.h" line="274"/>
         <source>数据台</source>
         <translation type="unfinished"></translation>
     </message>
@@ -62,6 +70,7 @@
         <location filename="mainwindow.ui" line="134"/>
         <location filename="cmake-build-debug-visual-studio/UdpHelper_autogen/include_Debug/ui_mainwindow.h" line="257"/>
         <location filename="cmake-build-release/UdpHelper_autogen/include_Release/ui_mainwindow.h" line="276"/>
+        <location filename="cmake-build-release-mingw_qt/UdpHelper_autogen/include/ui_mainwindow.h" line="276"/>
         <source>名称</source>
         <translation type="unfinished"></translation>
     </message>
@@ -69,6 +78,7 @@
         <location filename="mainwindow.ui" line="139"/>
         <location filename="cmake-build-debug-visual-studio/UdpHelper_autogen/include_Debug/ui_mainwindow.h" line="259"/>
         <location filename="cmake-build-release/UdpHelper_autogen/include_Release/ui_mainwindow.h" line="278"/>
+        <location filename="cmake-build-release-mingw_qt/UdpHelper_autogen/include/ui_mainwindow.h" line="278"/>
         <source>类型</source>
         <translation type="unfinished"></translation>
     </message>
@@ -76,6 +86,7 @@
         <location filename="mainwindow.ui" line="144"/>
         <location filename="cmake-build-debug-visual-studio/UdpHelper_autogen/include_Debug/ui_mainwindow.h" line="261"/>
         <location filename="cmake-build-release/UdpHelper_autogen/include_Release/ui_mainwindow.h" line="280"/>
+        <location filename="cmake-build-release-mingw_qt/UdpHelper_autogen/include/ui_mainwindow.h" line="280"/>
         <source>值</source>
         <translation type="unfinished"></translation>
     </message>
@@ -83,6 +94,7 @@
         <location filename="mainwindow.ui" line="149"/>
         <location filename="cmake-build-debug-visual-studio/UdpHelper_autogen/include_Debug/ui_mainwindow.h" line="263"/>
         <location filename="cmake-build-release/UdpHelper_autogen/include_Release/ui_mainwindow.h" line="282"/>
+        <location filename="cmake-build-release-mingw_qt/UdpHelper_autogen/include/ui_mainwindow.h" line="282"/>
         <source>长度</source>
         <translation type="unfinished"></translation>
     </message>
@@ -90,6 +102,7 @@
         <location filename="mainwindow.ui" line="164"/>
         <location filename="cmake-build-debug-visual-studio/UdpHelper_autogen/include_Debug/ui_mainwindow.h" line="264"/>
         <location filename="cmake-build-release/UdpHelper_autogen/include_Release/ui_mainwindow.h" line="283"/>
+        <location filename="cmake-build-release-mingw_qt/UdpHelper_autogen/include/ui_mainwindow.h" line="283"/>
         <source>总字节数：</source>
         <translation type="unfinished"></translation>
     </message>
@@ -97,6 +110,7 @@
         <location filename="mainwindow.ui" line="171"/>
         <location filename="cmake-build-debug-visual-studio/UdpHelper_autogen/include_Debug/ui_mainwindow.h" line="265"/>
         <location filename="cmake-build-release/UdpHelper_autogen/include_Release/ui_mainwindow.h" line="284"/>
+        <location filename="cmake-build-release-mingw_qt/UdpHelper_autogen/include/ui_mainwindow.h" line="284"/>
         <source>刷新</source>
         <translation type="unfinished"></translation>
     </message>
@@ -104,6 +118,7 @@
         <location filename="mainwindow.ui" line="189"/>
         <location filename="cmake-build-debug-visual-studio/UdpHelper_autogen/include_Debug/ui_mainwindow.h" line="266"/>
         <location filename="cmake-build-release/UdpHelper_autogen/include_Release/ui_mainwindow.h" line="285"/>
+        <location filename="cmake-build-release-mingw_qt/UdpHelper_autogen/include/ui_mainwindow.h" line="285"/>
         <source>添加字段</source>
         <translation type="unfinished"></translation>
     </message>
@@ -111,6 +126,7 @@
         <location filename="mainwindow.ui" line="196"/>
         <location filename="cmake-build-debug-visual-studio/UdpHelper_autogen/include_Debug/ui_mainwindow.h" line="267"/>
         <location filename="cmake-build-release/UdpHelper_autogen/include_Release/ui_mainwindow.h" line="286"/>
+        <location filename="cmake-build-release-mingw_qt/UdpHelper_autogen/include/ui_mainwindow.h" line="286"/>
         <source>删除字段</source>
         <translation type="unfinished"></translation>
     </message>
@@ -118,6 +134,7 @@
         <location filename="mainwindow.ui" line="203"/>
         <location filename="cmake-build-debug-visual-studio/UdpHelper_autogen/include_Debug/ui_mainwindow.h" line="268"/>
         <location filename="cmake-build-release/UdpHelper_autogen/include_Release/ui_mainwindow.h" line="287"/>
+        <location filename="cmake-build-release-mingw_qt/UdpHelper_autogen/include/ui_mainwindow.h" line="287"/>
         <source>写入消息框</source>
         <translation type="unfinished"></translation>
     </message>
@@ -125,6 +142,7 @@
         <location filename="mainwindow.ui" line="210"/>
         <location filename="cmake-build-debug-visual-studio/UdpHelper_autogen/include_Debug/ui_mainwindow.h" line="269"/>
         <location filename="cmake-build-release/UdpHelper_autogen/include_Release/ui_mainwindow.h" line="288"/>
+        <location filename="cmake-build-release-mingw_qt/UdpHelper_autogen/include/ui_mainwindow.h" line="288"/>
         <source>读取消息框</source>
         <translation type="unfinished"></translation>
     </message>
@@ -132,6 +150,7 @@
         <location filename="mainwindow.ui" line="232"/>
         <location filename="cmake-build-debug-visual-studio/UdpHelper_autogen/include_Debug/ui_mainwindow.h" line="270"/>
         <location filename="cmake-build-release/UdpHelper_autogen/include_Release/ui_mainwindow.h" line="289"/>
+        <location filename="cmake-build-release-mingw_qt/UdpHelper_autogen/include/ui_mainwindow.h" line="289"/>
         <source>保存模板</source>
         <translation type="unfinished"></translation>
     </message>
@@ -139,11 +158,12 @@
         <location filename="mainwindow.ui" line="239"/>
         <location filename="cmake-build-debug-visual-studio/UdpHelper_autogen/include_Debug/ui_mainwindow.h" line="271"/>
         <location filename="cmake-build-release/UdpHelper_autogen/include_Release/ui_mainwindow.h" line="290"/>
+        <location filename="cmake-build-release-mingw_qt/UdpHelper_autogen/include/ui_mainwindow.h" line="290"/>
         <source>载入模板</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="mainwindow.cpp" line="156"/>
+        <location filename="mainwindow.cpp" line="157"/>
         <source>载入文件</source>
         <translation type="unfinished"></translation>
     </message>
